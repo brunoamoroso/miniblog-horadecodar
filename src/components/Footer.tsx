30bf7c584dd8046129f06856671109c0ddf8d783
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-06-10 19:50:51 -0300

    started main container, footer and navbar
    Created the base for the main and footer, already finished the base for the navbar

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Mini Blog &copy; 2024</p>
+    </footer>
+  )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "./ui/navigation-menu";
+
+const menuLinks = [
+  {
+    text: 'Home',
+    to: '/'
+  },
+  {
+    text: 'Sobre',
+    to: '/about'
+  }
+];
+
+export default function Navbar() {
+  return (
+    <NavigationMenu className="max-w-full w-full border-b-2 px-14 justify-start backdrop-blur-sm"> 
+        <NavigationMenuList className="p-2">
+                {
+                  menuLinks.map((menuLink) => (
+                    <NavigationMenuItem className="py-2 px-4 text-slate-600 hover:text-slate-900 hover:bg-slate-100 rounded-sm">
+                      <NavigationMenuLink href={menuLink.to}>{menuLink.text}</NavigationMenuLink>
+                    </NavigationMenuItem>
+                  ))
+                }
+        </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
